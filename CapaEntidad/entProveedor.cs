bd31972bee22c7905f21994b4681892c3d97908e
Soy Nelson <juareznelsonelias@gmail.com>
2024-10-24 10:50:12 -0500

    Se agrego dos datos a la entidad proveedor

--- CapaEntidad/entProveedor.cs
+++ CapaEntidad/entProveedor.cs
@@ -14,7 +14,8 @@ namespace CapaEntidad
         public string Direccion { get; set; }
         public string Telefono { get; set; }
         public string Email { get; set; }
-
+        public DateTime FechaCreacion { get; set; }
+        public Boolean Estado {  get; set; }
 
     }
 }
